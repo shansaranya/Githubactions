4cab3596958851a954079ffa1f1b9ff84de6766d
shansaranya <147309479+shansaranya@users.noreply.github.com>
2025-11-01 14:00:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, world from my Docker container!\n');
+  res.end('Hello000, world from my Docker container!\n');
 });
 
 const port = 8080;
